52ea4ccaab6aec424c61b4ad87a5ed4ee130b59d
Jerome <chery.jerome@online.fr>
2017-06-21 19:42:49 +0200

    Key

--- main.tf
+++ main.tf
@@ -58,6 +58,7 @@ resource "aws_instance" "myweb" {
   ami           = "ami-7d50491b"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.websg.id}"]
+  key_name      = "MyFirst"
 
   ebs_block_device{
     device_name = "/dev/sdf"
