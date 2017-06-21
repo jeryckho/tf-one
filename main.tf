f926b4afdef987ed5be80603cb60b9ec2044c501
Jerome <chery.jerome@online.fr>
2017-06-21 08:01:18 +0200

    disk

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 // docker run -it --rm -v /c/Users/jerome.chery/terraform:/data broadinstitute/terraform plan
+// docker run -it --rm -v /c/Users/Anguerand/terraform:/data broadinstitute/terraform plan
 
 variable "access_key" {}
 variable "secret_key" {}
@@ -15,4 +16,10 @@ provider "aws" {
 resource "aws_instance" "montest" {
   ami           = "ami-7d50491b"
   instance_type = "t2.micro"
+
+  ebs_block_device{
+    device_name = "/dev/sdf"
+    volume_size = 16
+    volume_type = "gp2"
+  }
 }
